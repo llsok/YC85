b2c67ff49a846c90e23c88a2bf7859d1bea161c4
liaoyan <liaoyan@chinasofti.com>
2019-11-30 18:00:36 +0800

    java oop 工程初始化提交

---
+++ C85-S1-Ply-java/src/Hello.java
@@ -0,0 +1,28 @@
+
+/**
+ * 这是我的第一个类
+ * @author hy2005zyx
+ */
+public class Hello {
+	
+	/**
+	 * 这是我的主方法
+	 * @param args
+	 */
+	public static void main(String[] args){
+		
+		// 单行注释
+		
+		/*
+		 * 这是
+		 * 多行
+		 * 注释
+		 */
+		
+		/**
+		 * 文档注释，用于对方法、类的说明
+		 */
+		System.out.println("Hello World!");
+	}
+
+}
