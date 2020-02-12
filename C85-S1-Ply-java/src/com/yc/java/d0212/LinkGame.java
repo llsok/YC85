e0e6485ff16725429ec6329a15864957a5d7fbba
liaoyan <liaoyan@chinasofti.com>
2020-02-12 21:22:51 +0800

    实现有一个拐点的连通 check1 方法

--- C85-S1-Ply-java/src/com/yc/java/d0212/LinkGame.java
+++ C85-S1-Ply-java/src/com/yc/java/d0212/LinkGame.java
@@ -22,7 +22,7 @@ public class LinkGame {
 	private Random random = new Random();
 
 	private int[][] board = new int[SIZE][SIZE];
-	
+
 	Scanner scanner = new Scanner(System.in);
 
 	/**
@@ -30,22 +30,22 @@ public class LinkGame {
 	 */
 	public void start() {
 		init();
-		
-		while(true) {
+
+		while (true) {
 			showBoard();
 			System.out.println("请输入4个坐标：");
 			int x1 = scanner.nextInt();
 			int y1 = scanner.nextInt();
 			int x2 = scanner.nextInt();
 			int y2 = scanner.nextInt();
-			boolean result = check(x1,y1,x2,y2);
-			if(result == true) {
+			boolean result = check1(x1, y1, x2, y2);
+			if (result == true) {
 				// 连通成功，成功消去2个图形（数字置零）
 				board[y1][x1] = 0;
 				board[y2][x2] = 0;
 			}
 		}
-		
+
 	}
 
 	/**
@@ -73,7 +73,7 @@ public class LinkGame {
 				board[y][x] = 5;
 			}
 		}*/
-		
+
 		// 计算循环的次数
 		int count = SIZE * SIZE / 2;
 		for (int i = 0; i < count; i++) {
@@ -121,10 +121,10 @@ public class LinkGame {
 		System.out.println("第二个点的值："+board[y2][x2]);*/
 
 		// 判断2个图形（数字）是否一致
-		if(board[y1][x1] != board[y2][x2]) {
+		if (board[y1][x1] != board[y2][x2]) {
 			return false;
 		}
-		
+
 		// 判断是否平行
 		if (y1 == y2) {
 			// 判断是否是紧贴在一起的元素
@@ -135,9 +135,14 @@ public class LinkGame {
 			/**
 			 * i += (x1 < x2 ? 1 : -1)   循环变量根据 x1 和 x2 的坐标来 加减
 			 */
-			for (   int i = x1    ;    i == x2   ;    i += (x1 < x2 ? 1 : -1)    ) {
+			/**
+			 * 要点： 判断 x1 、x2 的位置，决定x1 到 x2 是加1 还是 减1，例如：x1在 x2的左边，那么 x1 要加1 才能逐渐到达 x2的位置
+			 * offset 表示x1 到达 x2 ，坐标值在每次循环要要变化的值
+			 */
+			int offset = x1 < x2 ? 1 : -1;
+			for (int i = x1 + offset; i != x2; i += offset) {
 				// 如果 不等于 0 表示当前位置有一个 图形, 直接返回 false
-				if(  board[y1][i] != 0  ) {
+				if (board[y1][i] != 0) {
 					return false;
 				}
 			}
@@ -149,15 +154,60 @@ public class LinkGame {
 			if (y1 == y2 + 1 || y1 == y2 - 1) {
 				return true;
 			}
-			for (   int i = y1    ;    i == y2   ;    i += (y1 < y2 ? 1 : -1)    ) {
+			int offset = y1 < y2 ? 1 : -1;
+			for (int i = y1 + offset; i != y2; i += offset) {
 				// 如果 不等于 0 表示当前位置有一个 图形, 直接返回 false
-				if(  board[i][x1] != 0  ) {
+				if (board[i][x1] != 0) {
 					return false;
 				}
 			}
 			return true;
 		}
 
+		return false;
+	}
+	
+	/**
+	 * 判断有一个拐点的连线是否连通
+	 * @param x1
+	 * @param y1
+	 * @param x2
+	 * @param y2
+	 * @return
+	 */
+	public boolean check1(int x1, int y1, int x2, int y2) {
+		// 判断2个图形（数字）是否一致
+		if (board[y1][x1] != board[y2][x2]) {
+			return false;
+		}
+		// 同行或同列的请求，直接调用 check
+		if(x1== x2 || y1 == y2) {
+			return check(x1, y1, x2, y2);
+		}
+		// 判断两个点与其中一个拐点（必须是0）是否同时连通
+		if(board[x1][y2] == 0) {
+			// 临时将拐点的值设置为当前的图形值
+			board[x1][y2] = board[x1][y1];
+			boolean b1 = check(x1, y1, x1, y2);
+			boolean b2 = check(x2, y2, x1, y2);
+			board[x1][y2] = 0;
+			if( b1 && b2) {
+				return true;
+			}
+		}
+		
+		// 判断两个点与另一个拐点（必须是0）是否同时连通
+		if(board[x2][y1] == 0) {
+			// 临时将拐点的值设置为当前的图形值
+			board[x2][y1] = board[x1][y1];
+			boolean b1 = check(x1, y1, x2, y1);
+			boolean b2 = check(x2, y2, x2, y1);
+			board[x2][y1] = 0;
+			if( b1 && b2) {
+				return true;
+			}
+		}
+		
 		return false;
 	}
 
