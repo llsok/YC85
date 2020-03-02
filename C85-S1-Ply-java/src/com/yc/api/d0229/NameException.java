c4a1a0ba196dcd1dc0fac997be301bef091a1f69
6606 <306529917@qq.com>
2020-03-02 14:25:25 +0800

    新增：随机取名作业参考

---
+++ C85-S1-Ply-java/src/com/yc/api/d0229/NameException.java
@@ -0,0 +1,37 @@
+package com.yc.api.d0229;
+
+public class NameException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NameException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NameException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NameException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NameException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NameException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+}
