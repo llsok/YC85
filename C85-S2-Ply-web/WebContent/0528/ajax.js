5ffecb527c33caba23b2b5c613360df2c2cd8b05
6606 <306529917@qq.com>
2020-05-29 14:58:26 +0800

    修改 ajax 参数判断bug

--- C85-S2-Ply-web/WebContent/0528/ajax.js
+++ C85-S2-Ply-web/WebContent/0528/ajax.js
@@ -12,7 +12,11 @@ function post(url, param, callback){
 	}
 	xhr.open("POST",url) // 开启连接   第三个参数 true异步|false同步
 	// 如果是文件上传方式, 请求不要设置 Content-type, 因为太复杂
-	if( ! param instanceof FormData) {
+	/**
+	 * if( ! param instanceof FormData) {  这里判断错了
+	 * js 特有的问题, js 里面任何类型都可以当成  boolean 使用, 影响了运算优先级, java 里面没有这个问题
+	 */
+	if( ! (param instanceof FormData)) {
 		// POST提交数据的编码方式
 		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	}
