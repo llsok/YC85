cf5a31059b22ddc4a2917a02c2a1007b2400d564
6606 <306529917@qq.com>
2020-07-12 14:55:38 +0800

    解决上传图片被eclipse清空的问题, 解决方法是将文件上传到 webapps/ROOT/upload

--- C85-S3-Ply-damai/src/com/yc/damai/web/UploadServlet.java
+++ C85-S3-Ply-damai/src/com/yc/damai/web/UploadServlet.java
@@ -1,5 +1,6 @@
 package com.yc.damai.web;
 
+import java.io.File;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -26,8 +27,21 @@ public class UploadServlet extends HttpServlet {
 		response.setContentType("text/html;charset=utf-8");
 		
 		Part part = request.getPart("file");
-		String webpath = "/products/upload";
+		
+		// 获取工程根目录  tomcat服务/webapps/工程名
+		String webpath = "/";
 		String diskpath = request.getServletContext().getRealPath(webpath);
+		// 将路径转成 File 对象
+		File proDir = new File(diskpath);
+		System.out.println(proDir);
+		// 获取上级的上级目录   tomcat
+		File tomcatDir = proDir.getParentFile().getParentFile();
+		// 获取上级目录   webapps/ROOT/upload
+		File uploadDir = new File(tomcatDir,"/webapps/ROOT/upload");
+		System.out.println(uploadDir);
+		// 重新赋值磁盘路径
+		diskpath = uploadDir.getAbsolutePath();
+		
 		System.out.println(diskpath);
 		diskpath = diskpath + "/" + part.getSubmittedFileName();
 		webpath += "/" + part.getSubmittedFileName();
@@ -36,7 +50,7 @@ public class UploadServlet extends HttpServlet {
 		part.write(diskpath);
 
 		Gson gson = new Gson();
-		Result res = new Result( 1, "文件上传成功!", webpath);
+		Result res = new Result( 1, "文件上传成功!", "/upload/" + part.getSubmittedFileName());
 		String json = gson.toJson(res);
 		response.getWriter().append(json);
 	}
