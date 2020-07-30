46c4babf7dc49dfad77ab8d2f3f6e40e612b2e2f
6606 <306529917@qq.com>
2020-07-30 10:42:05 +0800

    cookie的有效时间设置

--- C85-S3-Ply-thread/src/com/yc/thread/d0730/Cookie.java
+++ C85-S3-Ply-thread/src/com/yc/thread/d0730/Cookie.java
@@ -4,6 +4,7 @@ public class Cookie {
 
 	private String name;
 	private String value;
+	private int maxAge = -1;
 
 	public Cookie(String name, String value) {
 		this.name = name;
@@ -11,9 +12,18 @@ public class Cookie {
 	}
 
 	public String toString() {
-		String s = "Set-Cookie: %s=%s;\n";
+		String s = "Set-Cookie: %s=%s;";
 		s = String.format(s, name, value);
+		if(maxAge > -1) {
+			s += "Max-Age=" + maxAge + ";";
+		}
+		s+="\n";
+		System.out.println("s="+s);
 		return s;
+	}
+
+	public void setMaxAge(int maxAge) {
+		this.maxAge = maxAge;
 	}
 
 }
--- C85-S3-Ply-thread/src/com/yc/thread/d0730/CookieServlet.java
+++ C85-S3-Ply-thread/src/com/yc/thread/d0730/CookieServlet.java
@@ -7,6 +7,8 @@ public class CookieServlet extends HttpServlet{
 		 * 	向浏览器发送Cookie 值 name=wusong
 		 */
 		Cookie cookie = new Cookie("name","wusong");
+		cookie.setMaxAge(60 * 60 * 24);
+		
 		response.addCookie(cookie);
 		
 		response.getWriter().append("cookie");
