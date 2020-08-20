ea6e27887df22b445e123b662ee498f6efa65d19
6606 <306529917@qq.com>
2020-08-20 09:51:00 +0800

    SpringBoot Hello World

---
+++ C85-S3-Ply-SpringBoot/src/main/java/com/yc/C85S3PlySpringBoot/C85S3PlySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.yc.C85S3PlySpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C85S3PlySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C85S3PlySpringBootApplication.class, args);
+	}
+
+}
---
+++ C85-S3-Ply-SpringBoot/src/test/java/com/yc/C85S3PlySpringBoot/C85S3PlySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yc.C85S3PlySpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C85S3PlySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
