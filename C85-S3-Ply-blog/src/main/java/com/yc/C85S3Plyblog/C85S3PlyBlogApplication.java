d6a78764d1647771c67da4aa9b25917e7a467002
6606 <306529917@qq.com>
2020-08-23 15:50:54 +0800

    查询首页文章(未分页)

--- C85-S3-Ply-blog/src/main/java/com/yc/C85S3Plyblog/C85S3PlyBlogApplication.java
+++ C85-S3-Ply-blog/src/main/java/com/yc/C85S3Plyblog/C85S3PlyBlogApplication.java
@@ -1,9 +1,11 @@
 package com.yc.C85S3Plyblog;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.yc.C85S3Plyblog.dao")
 public class C85S3PlyBlogApplication {
 
 	public static void main(String[] args) {
