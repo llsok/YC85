f1ea68b588fc4e4e843ae2cd03c728c94dbc7728
6606 <306529917@qq.com>
2020-08-26 14:47:17 +0800

    日志+拦截器

--- C85-S3-Ply-blog/src/main/java/com/yc/C85S3Plyblog/C85S3PlyBlogApplication.java
+++ C85-S3-Ply-blog/src/main/java/com/yc/C85S3Plyblog/C85S3PlyBlogApplication.java
@@ -3,13 +3,33 @@ package com.yc.C85S3Plyblog;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.yc.C85S3Plyblog.web.LoginInterceptor;
 
 @SpringBootApplication
 @MapperScan("com.yc.C85S3Plyblog.dao")
-public class C85S3PlyBlogApplication {
+public class C85S3PlyBlogApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(C85S3PlyBlogApplication.class, args);
+	}
+
+	/**
+	 * 	拦截器注册方法
+	 * 	参数: 拦截器注册器
+	 */
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		WebMvcConfigurer.super.addInterceptors(registry);
+
+		InterceptorRegistration ir = registry.addInterceptor(new LoginInterceptor());
+		// 互联网项目 ==> 拦截少数资源
+		// 企业内部管理系统 ==> 拦截多数资源
+		ir.addPathPatterns("/toAddArticle", "/addArticle.do");
+
 	}
 
 }
---
+++ C85-S3-Ply-blog/src/main/java/com/yc/C85S3Plyblog/web/LoginInterceptor.java
@@ -0,0 +1,36 @@
+package com.yc.C85S3Plyblog.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class LoginInterceptor implements HandlerInterceptor{
+
+	/**
+	 * 	是在请求到控制器之前执行, 返回false之终止方法
+	 */
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// 判断当前是否登录
+				if (request.getSession().getAttribute("loginedUser") == null) {
+					// 根据当前的请求的方式返回不同结果
+					// 判断当前是 页面跳转还是 ajax 请求
+					String accept = request.getHeader("Accept");
+					if(accept.startsWith("application/json")) {
+						// ajax 请求
+						response.setContentType("application/json;charset=utf-8");
+						response.getWriter().append("{code:0,msg:'请先登录系统'}");
+					} else {
+						// 页面跳转请求
+						response.setContentType("text/html;charset=utf-8");
+						response.sendRedirect("/?mustLogin");
+					}
+					return false;
+				}
+				// 正常业务执行返回 true
+				return true;
+	}
+
+}
