b2ea4fab78e6a6abc32445d771b6933788971ebf
6606 <306529917@qq.com>
2020-08-27 10:17:39 +0800

    订单远程调用用户接口

--- C85-S3-Ply-cloud-order/src/main/java/com/yc/order/web/OrderAction.java
+++ C85-S3-Ply-cloud-order/src/main/java/com/yc/order/web/OrderAction.java
@@ -1,12 +1,17 @@
 package com.yc.order.web;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class OrderAction {
+	
+	@Resource
+	private RestTemplate restTemplate;
 	
 	@GetMapping("order")
 	public String order(HttpServletRequest req) {
@@ -14,5 +19,14 @@ public class OrderAction {
 				req.getLocalAddr(),
 				req.getLocalPort());
 	}
+	
+	@GetMapping("user")
+	public String user() {
+		String url="http://127.0.0.1:8001/user";
+		String res = restTemplate.getForObject(url, String.class);
+		return res;
+	}
+	
+
 
 }
