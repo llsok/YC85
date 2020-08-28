f35344d1b92a74376a4ec2ba316259408cc8ae0e
6606 <306529917@qq.com>
2020-08-28 16:37:30 +0800

    Zuul 路由网关配置

---
+++ C85-S3-Ply-cloud-zuul/src/main/java/com/yc/zuul/ZuulApplication.java
@@ -0,0 +1,31 @@
+package com.yc.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// zuul网关注解
+@EnableZuulServer
+@EnableEurekaClient
+@SpringBootApplication
+public class ZuulApplication implements WebMvcConfigurer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+	
+	/**
+	 * 	定义 RestTemplate  Bean
+	 */
+	@LoadBalanced
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+}
